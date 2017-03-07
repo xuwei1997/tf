32d9e27e7317acb11468e902024e0fe93580c078
xuwei1997 <xuwei@xu-wei.cn>
2017-03-07 21:40:22 +0800

    2

--- dl1.py
+++ dl1.py
@@ -6,8 +6,10 @@ import numpy as np
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 print(x_train, y_train)
+kk=x_train.shape
+print(kk)
 print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-print(x_train, y_train)
+print(x_test, y_test)
 
 model= Sequential()
 model.add (Dense(input_dim=28*28,output_dim=500))
@@ -19,8 +21,11 @@ model.add(Activation('sigmoid'))
 model.compile(loss='mse',optimizer=SGD(lr=0.1),metrics=['accuracy'])
 
 a=0
-for x in x_train:
+for x0 in x_train:
 	y=y_train[a]
+	x = x0.reshape((1,784))
+	k=x.shape
+	print(k)
 	model.fit(x,y,batch_size=100,nb_epoch=20)
 	score=model.evaluate(x_text,y_test)
 	print(score[0])
